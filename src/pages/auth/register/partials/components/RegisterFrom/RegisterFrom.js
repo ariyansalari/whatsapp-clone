cd3a05618b13a84412decf2870c128fc32b703f8
ariyansalari <ariyan4509@gmail.com>
2024-03-09 09:51:18 -0800

    feat(page): implement-authpage

--- src/pages/auth/register/partials/components/RegisterFrom/RegisterFrom.js
+++ src/pages/auth/register/partials/components/RegisterFrom/RegisterFrom.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 
 export const RegisterFrom = () => {
   const { handleSubmit, register, registerData, errors } = useRegister();
-  const { status } = useSelector((state) => state.user);
+  const { status ,error} = useSelector((state) => state.user);
   return (
     <div className="h-screen w-full flex items-center justify-center overflow-hidden">
       <div className="max-w-md space-y-8 p-10 dark:bg-dark_bg_2 rounded-xl ">
@@ -43,7 +43,9 @@ export const RegisterFrom = () => {
             placeholder={"password:"}
             type={"password"}
           />
-
+{error? (<div>
+  <p className="text-red-400">{error}</p>
+</div>):null}
           <button
             className="w-full flex justify-center bg-green_1 text-gray-100 p-4 rounded-full tracking-wide font-semibold focus:outline-none hover:bg-green_2 shadow-lg cursor-pointer transition ease-in duration-300"
             type="submit"
