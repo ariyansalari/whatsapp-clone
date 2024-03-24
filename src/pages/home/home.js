474bb6e9a46d137a3f02d32cc545fcae2befc999
ariyansalari <ariyan4509@gmail.com>
2024-03-24 02:56:37 -0700

    feat(screen): implement-sideBar

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,11 +2,14 @@ import { SideBar } from "components";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getConversations } from "system";
+import { WhatsappHome } from "./partials";
 
 export const HomeScreen = () => {
   const dispatch=useDispatch()
 
 const {user}=useSelector((state)=>state.user)
+const {activeConversation}=useSelector((state)=>state.chat)
+console.log(activeConversation);
 
   useEffect(()=>{
 if(user){
@@ -18,6 +21,7 @@ if(user){
     <div className="h-screen dark:bg-dark_bg_1  flex items-center justify-center py-[19px] overflow-hidden">
     <div className="container h-screen flex">
       <SideBar/>
+      {activeConversation.length>0?'home':<WhatsappHome/>}
     </div>
     </div>
   );
---
+++ src/pages/home/partials/WhatsappHome/WhatsappHome.jsx
@@ -0,0 +1,22 @@
+import { Logo } from "assets";
+
+export const WhatsappHome = () => {
+  return (
+    <div className="h-full w-full dark:bg-dark_bg_4 select-none border-l border-l-dark_border_2 border-b-[6px]  border-b-green_2">
+      <div className="-mt-1.5 w-full h-full flex flex-col gap-y-8 items-center justify-center">
+        <span>
+          <Logo />
+        </span>
+        <div className="mt-1 text-center space-y-[12px]">
+          <h1 className="text-[32px] dark:text-dark_text_4 font-extralight">
+            Whatsapp Web
+          </h1>
+          <p className="text-sm dark:text-dark_text_3">
+            Send and receive messages without keeping your phone online. <br />
+            Use Whatsapp on up to 4 linked devices and 1 phone at the same time.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
